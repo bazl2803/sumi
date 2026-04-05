28d5b81f189aa3708dcaa2a8bb09161a78a825dc
Bryan Lemus <bryanalelemus@gmail.com>
2026-04-04 21:26:17 -0600

    Fix incorect client baseURL value

--- src/app/(store)/_components/profile-button.tsx
+++ src/app/(store)/_components/profile-button.tsx
@@ -1,38 +1,28 @@
-import { Avatar, Button } from '@/components'
-import { User } from 'better-auth'
-import { css, sva } from 'panda/css'
+import { Avatar, Button } from "@/components";
+import { User } from "better-auth";
+import { css } from "panda/css";
 
 // --- Types ------------------------------------------------------------------
 interface ProfileButtonProps {
-	user: User
+  user: User;
 }
 
 // --- Component ---------------------------------------------------------------
 export const ProfileButton = ({ user }: ProfileButtonProps) => {
-	return (
-		<>
-			<div className={css({ display: 'none', lg: { display: 'block' } })}>
-				<Button
-					rounded={'full'}
-					variant={'subtle'}
-				>
-					<Button.Addon>
-						<Avatar
-							src={user.image!}
-							fallback={user.name![0]}
-						/>
-					</Button.Addon>
-					<Button.Label>{user.name}</Button.Label>
-				</Button>
-			</div>
+  return (
+    <>
+      <div className={css({ display: "none", lg: { display: "block" } })}>
+        <Button rounded={"full"} variant={"subtle"}>
+          <Button.Addon>
+            <Avatar src={user.image!} fallback={user.name![0]} />
+          </Button.Addon>
+          <Button.Label>{user.name}</Button.Label>
+        </Button>
+      </div>
 
-			<div className={css({ display: 'block', lg: { display: 'none' } })}>
-				<Avatar
-					size='sm'
-					src={user.image!}
-					fallback={user.name![0]}
-				/>
-			</div>
-		</>
-	)
-}
+      <div className={css({ display: "block", lg: { display: "none" } })}>
+        <Avatar size="sm" src={user.image!} fallback={user.name![0]} />
+      </div>
+    </>
+  );
+};
--- src/app/(store)/_components/store-appbar.tsx
+++ src/app/(store)/_components/store-appbar.tsx
@@ -1,82 +1,82 @@
-'use client'
+"use client";
 
-import { Appbar, Button, ButtonIcon, CommandBox, Group, Logo } from '@/components'
-import { IconSearch, IconShoppingCart } from '@tabler/icons-react'
-import { css, cx, sva } from 'panda/css'
-import { AccountControl } from './account-control'
-import { StoreCommandBox } from '@/app/(store)/_components/store-command-box'
+import { Appbar, Button, ButtonIcon, Group, Logo } from "@/components";
+import { IconShoppingCart } from "@tabler/icons-react";
+import { sva } from "panda/css";
+import { AccountControl } from "./account-control";
+import { StoreCommandBox } from "@/app/(store)/_components/store-command-box";
 
 const styles = sva({
-	slots: ['root', 'commandBox', 'wrapper', 'shoppingCartButton'],
-	base: {
-		root: {
-			display: 'flex',
-			flexWrap: 'nowrap',
-			alignItems: 'top',
-			flexDirection: 'row',
-			position: 'sticky',
-			top: 0,
-			zIndex: '100',
-			paddingBlock: 4,
-			paddingBottom: 8,
-			overflowY: 'visible',
-			bgLinear: 'to-b',
-			gradientFrom: { base: 'neutral.50', _osDark: 'neutral.950' },
-			gradientVia: { base: 'neutral.50/50', _osDark: 'neutral.950/50' },
-			gradientViaPosition: '75%',
-			gradientTo: { base: 'neutral.50/0', _osDark: 'neutral.950/0' },
-			gap: 4,
-		},
-		commandBox: {
-			display: 'none',
-			lg: {
-				display: 'block',
-				width: 'lg',
-			},
-		},
-		shoppingCartButton: {
-			display: 'none',
-			lg: {
-				display: 'block',
-			},
-		},
-		wrapper: {
-			display: 'flex',
-			flexWrap: 'nowrap',
-			alignItems: 'center',
-			justifyContent: 'space-between',
-			gap: 4,
-			paddingInline: 4,
-			m: 'auto',
-			w: 'full',
-			maxW: '7xl',
-		},
-	},
-})
+  slots: ["root", "commandBox", "wrapper", "shoppingCartButton"],
+  base: {
+    root: {
+      display: "flex",
+      flexWrap: "nowrap",
+      alignItems: "top",
+      flexDirection: "row",
+      position: "sticky",
+      top: 0,
+      zIndex: "100",
+      paddingBlock: 4,
+      paddingBottom: 8,
+      overflowY: "visible",
+      bgLinear: "to-b",
+      gradientFrom: { base: "neutral.50", _osDark: "neutral.950" },
+      gradientVia: { base: "neutral.50/50", _osDark: "neutral.950/50" },
+      gradientViaPosition: "75%",
+      gradientTo: { base: "neutral.50/0", _osDark: "neutral.950/0" },
+      gap: 4,
+    },
+    commandBox: {
+      display: "none",
+      lg: {
+        display: "block",
+        width: "lg",
+      },
+    },
+    shoppingCartButton: {
+      display: "none",
+      lg: {
+        display: "block",
+      },
+    },
+    wrapper: {
+      display: "flex",
+      flexWrap: "nowrap",
+      alignItems: "center",
+      justifyContent: "space-between",
+      gap: 4,
+      paddingInline: 4,
+      m: "auto",
+      w: "full",
+      maxW: "7xl",
+    },
+  },
+});
 
 export const StoreAppbar = ({ className }: { className?: string }) => {
-	const classes = styles()
+  const classes = styles();
 
-	return (
-		<Appbar>
-			<Appbar.Line>
-				<Logo />
+  return (
+    <Appbar>
+      <Appbar.Line>
+        <Logo />
 
-				<StoreCommandBox className={classes.commandBox} />
+        <StoreCommandBox className={classes.commandBox} />
 
-				<Group>
-					<Button
-						className={classes.shoppingCartButton}
-						variant="subtle"
-						rounded="full"
-						size="icon"
-					>
-						<ButtonIcon icon={IconShoppingCart} />
-					</Button>
+        <Group>
+          <Button
+            className={classes.shoppingCartButton}
+            variant="subtle"
+            rounded="full"
+            size="icon"
+          >
+            <ButtonIcon icon={IconShoppingCart} />
+          </Button>
 
-					<AccountControl />
-				</Group>
-			</Appbar.Line>
-		</Appbar>
-	)
-}
+          <AccountControl />
+        </Group>
+      </Appbar.Line>
+    </Appbar>
+  );
+};
